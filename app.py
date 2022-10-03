0008af897c686bf0f23b15e7678b0da56c75a763
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-10-03 16:23:05 -0600

    add templates, remove unnecessary endpoints

--- app.py
+++ app.py
@@ -16,18 +16,6 @@ def status():
 def upload_file_template():
    return render_template('upload.html')
 
-# @app.route('/file_list')
-# def file_list():
-#     f_list = os.listdir(upload_folder)
-#     return render_template('file_list.html', f_list = f_list)
-
-#@app.route('/login', methods = ["GET", "POST"])
-#def fj_user():
-    #if request.method == "POST":
-        #fj_user = request.form.get("username2")
-        #fj_pass = request.form.get("pwd2")
-    #return render_template('upload_success.html', file_uploaded=secure_filename(f.filename))
-
 	
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
@@ -41,19 +29,3 @@ def upload_file():
       #fj_user = "username2"
       #fj_pass = "pwd2"
       return render_template('upload_success.html', file_uploaded=secure_filename(f.filename))
-
-# @app.route('/delete/<file_name>')
-# def delete(file_name):
-#     try:
-#         os.remove(os.path.join(upload_folder, secure_filename(file_name)))
-#         return render_template('file_deleted.html', file_name=secure_filename(file_name))
-#     except:
-#         return render_template('Static_File_Not_Found.html', file_name=secure_filename(file_name)), 404
-
-
-# @app.route('/download/<file_name>')
-# def download(file_name):
-#     try:
-#         return flask.send_from_directory(upload_folder, secure_filename(file_name))
-#     except:
-#         return render_template('Static_File_Not_Found.html', file_name=secure_filename(file_name)), 404
