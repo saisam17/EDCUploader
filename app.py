9d22865779c225af949fc1975938f3f198affba5
saisam17 <saisam17@gmail.com>
2022-10-04 16:17:48 -0600

    Updated uploader and ensured form data responses are working

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os, flask
+# noinspection PyUnresolvedReferences
+import excel2flapjack as e2f
+#import excel2sbol
 
 cwd = os.getcwd()
 upload_folder = os.path.join(cwd,'public')
@@ -10,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/status')
 def status():
-    return("The Public Server for plugin testing is up and running")
+    return("The Public Server for the EDC is up and running")
 
 @app.route('/upload')
 def upload_file_template():
@@ -20,14 +23,24 @@ def upload_file_template():
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
-      #hashmap = excel2flapjack.upload(f, pass, username)
-      #file = excel2sbol(f)
+      fj_url = "flapjack.rudge-lab.org:8000"
+      fj_user = request.form['fjusername']
+      fj_pass = request.form['fjpwd']
+      with open(request.files['file']) as f:
+         hash_map=e2f.main.flapjack_upload(fj_url, fj_user, fj_pass, f)
+         print(hash_map)
+
+      #f = request.files['file']
+
+      # hashmap = excel2flapjack.upload(f, fj_user, fj_pass)
+      # file = excel2sbol(f)
       #linked_file = addflapjacklinks(file)
       #submit linked_file to synbiohub
       #f.save(os.path.join(upload_folder, secure_filename(f.filename)))
+      #print(f)
+      #print(request.form)
+      #print(request.form['sbhusername'])
+      return render_template('upload_success.html', file_uploaded=secure_filename(f.filename))
+
       #fj_user = "username2"
       #fj_pass = "pwd2"
-      print(request.form)
-      print(request.form['sbhusername'])
-      return render_template('upload_success.html', file_uploaded=secure_filename(f.filename))
